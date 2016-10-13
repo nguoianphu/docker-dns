eed4ef8fbefa50f5208ef2b2c7861cfbe98b4c4b
Nguoi An Phu <nguoianphu@gmail.com>
2016-10-13 15:12:40 +0700

    start named

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-set -e
+set -xe
 
 # If user don't provide any command
 # start the BIND server
 # Run as user "bind"
 if [[ "$1" == "" ]]; then
     echo "Starting BIND"
-    exec gosu bind named
+    # exec gosu bind named
+    exec bind named -g
 else
     # Else allow the user to run arbitrarily commands like bash
     exec "$@"
